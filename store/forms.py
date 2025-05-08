f48987ca9b33a47bdc3f2e9d0bca390cfdb41192
isaacaisha <medusadbt@gmail.com>
2025-05-08 05:53:22 +0000

    added udpdate user information fonctionality

--- store/forms.py
+++ store/forms.py
@@ -1,21 +1,54 @@
 # /home/siisi/e-commerce/store/forms.py
 
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPasswordForm
 from django import forms
+from .models import Profile
+
+
+class UserInfoForm(forms.ModelForm):
+	phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), required=False)
+	address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 1'}), required=False)
+	address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 2'}), required=False)
+	city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=False)
+	state = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}), required=False)
+	zipcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zipcode'}), required=False)
+	country = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Country'}), required=False)
+
+	class Meta:
+		model = Profile
+		fields = ('phone', 'address1', 'address2', 'city', 'state', 'zipcode', 'country')
+
+
+class ChangePasswordForm(SetPasswordForm):
+	class Meta:
+		model: User
+		fields = ['new_password1', 'new_password2']
+
+	def __init__(self, *args, **kwargs):
+		super(ChangePasswordForm, self).__init__(*args, **kwargs)
+
+		self.fields['new_password1'].widget.attrs['class'] = 'form-control'
+		self.fields['new_password1'].widget.attrs['placeholder'] = 'New Password'
+		self.fields['new_password1'].label = ''
+		self.fields['new_password1'].help_text = ''
+
+		self.fields['new_password2'].widget.attrs['class'] = 'form-control'
+		self.fields['new_password2'].widget.attrs['placeholder'] = 'Confirm New Password'
+		self.fields['new_password2'].label = ''
+		self.fields['new_password2'].help_text = ''
 
 
 class UpdateUserForm(UserChangeForm):
-	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
-	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}), required=False)
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}), required=False)
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=False)
 
 	# Hide Password stuff
 	password = None
 
 	class Meta:
 		model = User
-		#fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 		fields = ('username', 'first_name', 'last_name', 'email')
 
 	def __init__(self, *args, **kwargs):
@@ -28,7 +61,7 @@ class UpdateUserForm(UserChangeForm):
 
 
 class SignUpForm(UserCreationForm):
-	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=False)
 
 	class Meta:
 		model = User
--- store/views.py
+++ store/views.py
@@ -1,15 +1,71 @@
 #/home/siisi/e-commerce/store/views.py
 
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 
 from datetime import datetime, timezone
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        info_form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if info_form.is_valid():
+            info_form.save()
+
+            #login(request, current_user)
+            messages.success(request, (
+                f"{request.user.username} Info Has Been Updated Successfully 👌🏿!"
+                ))
+            return redirect('update_info')
+        date = datetime.now(timezone.utc).strftime("%a %d %B %Y")
+        context = {
+            'info_form': info_form,
+            'date' : date
+        }
+        return render(request, 'update_info.html', context)
+    else:
+        messages.warning(request, "You must Be Logged In To Access That Page 🤣.")
+        return redirect('home')
+
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Did they fill out the password_form
+        if request.method == 'POST':
+            password_form = ChangePasswordForm(current_user, request.POST)
+            # Is the password_form valid
+            if password_form.is_valid():
+                password_form.save()
+                messages.success(request, (
+                    f"{current_user.username}, Your Password Has Been Updated Successfully 👌🏿!"
+                    ))
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                for error in list(password_form.errors.values()):
+                    messages.warning(request, error)
+                    return redirect('update_password')
+        else:
+            password_form = ChangePasswordForm(current_user)
+            date = datetime.now(timezone.utc).strftime("%a %d %B %Y")
+
+            context = {
+                'password_form': password_form,
+                'date' : date
+            }
+            return render(request, 'update_password.html', context)
+    else:
+        messages.warning(request, "You must Be Logged In To Access That Page 😭.")
+        return redirect('home')
 
 
 def update_user(request):
@@ -32,7 +88,7 @@ def update_user(request):
         }
         return render(request, 'update_user.html', context)
     else:
-        messages.success(request, "You must Be Logged In To Access That Page 🤣.")
+        messages.warning(request, "You must Be Logged In To Access That Page 🤣.")
         return redirect('home')
 
 
@@ -116,7 +172,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, (f"{user.username} Log In Successfully 👌🏿!"))
+            messages.success(request, (f"Welcome {user.username}, You've Been Log In Successfully 👌🏿!"))
             return redirect('home')
         else:
             messages.warning(request, ("There was an error, please try again. 😭."))
@@ -137,26 +193,26 @@ def logout_user(request):
 
 
 def register_user(request):
-    form = SignUpForm()
+    date = datetime.now(timezone.utc).strftime("%a %d %B %Y")
     if request.method == "POST":
         form = SignUpForm(request.POST)
-        if  form.is_valid():
-            form.save()
+        if form.is_valid():
+            user = form.save()
+            # Automatically create Profile via signal
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            # log in user
-            user = authenticate(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
             login(request, user)
-            messages.success(request, (f"{user.username} Successfully Registered 👌🏿!"))
-            return redirect('home')
+            messages.success(request, f"{user.username} Successfully Registered 👌🏿!")
+            return redirect('update_info')
         else:
-            messages.warning(request, ("Whoops! There was a problem Registering, please try again 🤣."))
-            return redirect('register')
+            # Form invalid: render with errors
+            messages.warning(request, "Please correct the errors below.")
     else:
-        date = datetime.now(timezone.utc).strftime("%a %d %B %Y")
+        form = SignUpForm()
 
-        context = {
-            'form' : form,
-            'date' : date
-        }
-        return render(request, 'register.html', context)
+    context = {
+        'form': form,
+        'date': date,
+    }
+    return render(request, 'register.html', context)
